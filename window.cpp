6fc614210c04996f05b15974d28441f0ecbf22fe
leweenie <tleween13@gmail.com>
2025-11-28 18:02:02 -0500

    feat: logic for moving shape

--- window.cpp
+++ window.cpp
@@ -1,26 +1,51 @@
 #include "rlImGui/rlImGui.h"
 #include <imgui.h>
 #include <raylib.h>
+#include <raymath.h>
 
 int main() {
     int screenWidth = 1920 / 2;
     int screenHeight = 1080 / 2;
     bool showMessage = false;
 
+    Vector2 circlePos = {100, 100};
+    Vector2 targetPos = circlePos;
+
     InitWindow(screenWidth, screenHeight, "Physics Engine");
     rlImGuiSetup(true);
 
+    SetTargetFPS(60);
+
     while (!WindowShouldClose()) {
+        float dt = GetFrameTime();
+        float speed = 500.0f;
+        float step = speed * dt;
+
+        Vector2 direction = Vector2Subtract(targetPos, circlePos);
+        float distance = Vector2Length(direction);
+
+        if (distance <= step) {
+            circlePos = targetPos;
+        } else {
+            Vector2 normalized = Vector2Scale(direction, 1.0 / distance);
+            circlePos =
+                Vector2Add(circlePos, Vector2Scale(normalized, speed * dt));
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
 
         const float halfScreenWidth = GetScreenWidth() * 0.5f;
         const float halfScreenHeight = GetScreenHeight() * 0.5f;
-        DrawCircle(halfScreenWidth, halfScreenHeight, 80, ORANGE);
+        DrawCircleV(circlePos, 80, ORANGE);
 
         if (showMessage) {
             DrawText("Example text: 'Hello, World!'", GetScreenWidth() / 2,
                      GetScreenHeight() / 2, 12, WHITE);
+        }
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            targetPos = GetMousePosition();
         }
 
         rlImGuiBegin();
